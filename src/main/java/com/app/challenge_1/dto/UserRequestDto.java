02139b0e59957c4534ebeb6b2223d3e49f9a61ce
sharadha <chelkasharadha91@gmail.com>
2024-05-07 09:46:34 +0530

    API Calls and added setters and getters

--- src/main/java/com/app/challenge_1/dto/UserRequestDto.java
+++ src/main/java/com/app/challenge_1/dto/UserRequestDto.java
@@ -2,6 +2,7 @@ package com.app.challenge_1.dto;
 
 import lombok.Data;
 
+//lombok is not working
 @Data
 public class UserRequestDto {
     private int id;
@@ -9,5 +10,38 @@ public class UserRequestDto {
     private String lastName;
     private String email;
     private String password;
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
 
 }
--- src/main/java/com/app/challenge_1/dto/UserResponseDto.java
+++ src/main/java/com/app/challenge_1/dto/UserResponseDto.java
@@ -12,5 +12,20 @@ public class UserResponseDto {
     private User user;
     //private Response response;
     private ResponseStatus responseStatus;
+    
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public ResponseStatus getResponseStatus() {
+		return responseStatus;
+	}
+	public void setResponseStatus(ResponseStatus responseStatus) {
+		this.responseStatus = responseStatus;
+	}
+    
+    
 
 }
